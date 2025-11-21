533a8046a72322fde9987fbbc95773086cfa6876
Alexandru Gologan <alexandru.gologan@gmail.com>
2025-11-21 10:55:34 +0200

    chore: update merged report now sorted

--- merge.sh
+++ merge.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-jq ".packages[].location" index.json | xargs ~/go/bin/vexctl merge --author "agologan@users.noreply.github.com" > merged.openvex.json
+# go install github.com/openvex/vexctl@latest
+jq ".packages[].location" index.json | \
+  xargs ~/go/bin/vexctl merge --author "agologan@users.noreply.github.com" | \
+  jq '.statements |= sort_by(.vulnerability["@id"])' | \
+  jq '.statements[] |= del(.timestamp)' > merged.openvex.json
